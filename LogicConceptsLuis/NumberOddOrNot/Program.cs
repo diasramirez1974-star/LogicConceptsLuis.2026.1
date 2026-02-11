cab52f4834e686333c959ebb474b5436bdcdd7ca
LUIS ALEJANDRO ALZATE RAMIREZ <luisalzate1158182@correo.itm.edu.co>
2026-02-11 13:09:23 -0500

    Excercise Complete

---
+++ LogicConceptsLuis/NumberOddOrNot/Program.cs
@@ -0,0 +1,16 @@
+﻿do
+{
+    Console.Write("Ingrese número:");
+    var numberString = Console.ReadLine();
+    var numberInt = int.Parse(numberString!);
+    if (numberInt % 2 == 0)
+    {
+        Console.WriteLine($"El numero {numberInt}, es par.");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {numberInt}, es impar.");
+    }
+}
+while (true);
+
