e3cc7dc7a58c9d20dec5f7016194ad3efacb3e58
LUIS ALEJANDRO ALZATE RAMIREZ <luisalzate1158182@correo.itm.edu.co>
2026-02-12 12:46:39 -0500

    Excercise complete

--- LogicConceptsLuis/NumberOddOrNot/Program.cs
+++ LogicConceptsLuis/NumberOddOrNot/Program.cs
@@ -1,16 +1,29 @@
-﻿do
+﻿var numberString = string.Empty;
+do
 {
-    Console.Write("Ingrese número:");
-    var numberString = Console.ReadLine();
-    var numberInt = int.Parse(numberString!);
-    if (numberInt % 2 == 0)
+    Console.Write("Ingrese número entero o 'S' para salir:");
+    numberString = Console.ReadLine();
+    if (numberString == "S")
     {
-        Console.WriteLine($"El numero {numberInt}, es par.");
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt}, es impar.");
+        }
     }
     else
     {
-        Console.WriteLine($"El numero {numberInt}, es impar.");
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un numero entero.");
     }
-}
-while (true);
+} while (numberString != "S");
+
 
