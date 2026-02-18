9974559ca1701466769ca7de0f35f89d1cecd0f4
LUIS ALEJANDRO ALZATE RAMIREZ <luisalzate1158182@correo.itm.edu.co>
2026-02-18 17:52:05 -0500

    Excercise complete 1.1

---
+++ LogicConceptsLuis/Highest_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Ingrese el primer número:");
+var number1String = Console.ReadLine();
+var number1Int = int.Parse(number1String!);
+Console.Write("Ingrese el segundo número:");
+var number2String = Console.ReadLine();
+var number2Int = int.Parse(number2String!);
+Console.Write("Ingrese el tercer número:");
+var number3String = Console.ReadLine();
+var number3Int = int.Parse(number3String!);
+if (number1Int > number2Int && number1Int > number3Int)
+{
+    Console.Write($"El número mayor es {number1Int}");
+}
+if (number2Int > number1Int && number2Int > number3Int)
+{
+    Console.Write($"El número mayor es {number2Int}");
+}
+if (number3Int > number1Int && number3Int > number2Int)
+{
+    Console.Write($"El número mayor es {number3Int}");
+}
