ed6b2f4ad9082e6ae83b19b16835fcfac49d4698
LUIS ALEJANDRO ALZATE RAMIREZ <luisalzate1158182@correo.itm.edu.co>
2026-02-20 16:11:54 -0500

    Excercise complete 2

---
+++ LogicConceptsLuis/ORDERING_OF_THREE_NUMBERS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Design;
+
+Console.Write("Ingrese el primer número:");
+var number1String = Console.ReadLine();
+var number1Int = int.Parse(number1String!);
+Console.Write("Ingrese el segundo número:");
+var number2String = Console.ReadLine();
+var number2Int = int.Parse(number2String!);
+Console.Write("Ingrese el tercer número:");
+var number3String = Console.ReadLine();
+var number3Int = int.Parse(number3String!);
+if (number1Int > number2Int && number1Int > number3Int)
+{
+    Console.Write($"El número mayor es {number1Int}");
+    if (number2Int < number1Int && number2Int > number3Int)
+    {
+        Console.Write($"El número del medio es {number2Int}");
+    }
+    else
+    {
+        Console.Write($"El número del medio es {number3Int}");
+    }
+}
+if (number2Int > number1Int && number2Int > number3Int)
+{
+    Console.Write($"El número mayor es {number2Int}");
+    if (number1Int < number2Int && number1Int > number3Int)
+    {
+        Console.Write($"El número del medio es {number1Int}");
+    }
+    else
+    {
+        Console.Write($"El número del medio es {number3Int}");
+    }
+}
+if (number3Int > number1Int && number3Int > number2Int)
+{
+    Console.Write($"El número mayor es {number3Int}");
+    if (number2Int < number3Int && number2Int > number1Int)
+    {
+        Console.Write($"El número del medio es {number2Int}");
+    }
+    else
+    {
+        Console.Write($"El número del medio es {number1Int}");
+    }
+}
